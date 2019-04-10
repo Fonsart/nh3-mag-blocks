820776dfdbabe43754d04816d8529e6d35d3019b
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-04-10 12:17:08 +0200

    Refactor gulpfile to match v4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,48 @@ const rename = require('gulp-rename');
 
 const pluginName = 'nh3-mag-archive-image-block';
 
-gulp.task('zip', () =>
-  gulp.src([
-    '*.php',
-    '{js,js/**/*}',
-    '{css,css/**/*}',
-    'languages/**/{*.po~,*.pot~}',
-  ])
+/**
+ * 'zip' task function.
+ *
+ * Create a zip file that contains the plugin files in a directory named after the plugin.
+ */
+function makeZip() {
+  return gulp
+    .src([
+      '*.php',
+      '{js,js/**/*}',
+      '{css,css/**/*}',
+      'languages/**/{*.po~,*.pot~}',
+    ])
     .pipe(rename((file) => file.dirname = `${pluginName}/${file.dirname}`))
     .pipe(zip(`${pluginName}.zip`))
     .pipe(gulp.dest('.'))
-)
+}
+
+/**
+ * 'css:build' task function.
+ *
+ * Currently only copy the css from the source forlder to the build folder.
+ */
+function copyCss() {
+  return gulp
+    .src('./src/css/**/*')
+    .pipe(gulp.dest('./build/css/'))
+}
+
+/**
+ * 'watch' task function.
+ *
+ * Watch for changes on the css source folder to trigger the copyCss function.
+ */
+function watch() {
+  return gulp
+    .watch('src/css/**/*', copyCss)
+}
+
+// Exports tasks to CLI
+module.exports = {
+  'css:build': copyCss,
+  'zip': makeZip,
+  watch
+};
