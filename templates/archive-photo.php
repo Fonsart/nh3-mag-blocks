6e40d53393963589e804e34bacea0b6a25e02093
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-04-25 16:36:30 +0200

    Replace __() by esc_html__() where necessary

--- templates/archive-photo.php
+++ templates/archive-photo.php
@@ -2,7 +2,7 @@
   <picture>
     <a href="<?php echo NH3_MAG_ARCHIVE_URL; ?>/entries/<?php echo esc_attr( $att['document_hash'] ); ?>" target="_blank">
       <img src="<?php echo esc_attr( $att['photo_thumbnail_url'] ); ?>"/>
-      <p class="post-block__media-credits">"<?php echo isset($att['photo_title']) ? $att['photo_title'] : __('Untitled', 'nh3-mag-archive-blocks') ?>", <?php _e('published by', 'nh3-mag-archive-blocks'); ?> <?php echo esc_html( $att['photo_author'] ); ?></p>
+      <p class="post-block__media-credits">"<?php echo isset($att['photo_title']) ? $att['photo_title'] : esc_html__('Untitled', 'nh3-mag-archive-blocks') ?>", <?php _e('published by', 'nh3-mag-archive-blocks'); ?> <?php echo esc_html( $att['photo_author'] ); ?></p>
     </a>
   </picture>
   <?php if (isset($att['photo_caption'])): ?>
