b7aecce8c0948c50b43b88c2cafe46c83ebc278d
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-05-21 13:11:12 +0200

    Hide caption and credit on front page

--- classes/class-nh3-blocks-featured-image.php
+++ classes/class-nh3-blocks-featured-image.php
@@ -5,11 +5,11 @@ class NH3_Blocks_Featured_Image {
   const CUSTOM_META_NAME = 'nh3_mag_custom_featured_image_field';
 
   public function __construct() {
-    add_filter( 'post_thumbnail_html', array($this, 'display_custom_featured_image'), 10, 2 );
+    add_filter( 'post_thumbnail_html', array($this, 'display_custom_featured_image'), 10, 4 );
     $this->register_custom_meta();
   }
 
-  public function display_custom_featured_image(string $html, int $post_id) {
+  public function display_custom_featured_image(string $html, int $post_id, string $post_thumbnail_id, string $size) {
     $att = json_decode(get_post_meta( $post_id, self::CUSTOM_META_NAME, true ), true);
     if ( isset( $att['fileUrl'] ) ) {
       ob_start();
--- templates/display-image.php
+++ templates/display-image.php
@@ -1,15 +1,17 @@
+<?php $nh3_blocks_with_caption = isset($att['caption']) && (!isset($size) || 'full' === $size); ?>
+<?php $nh3_blocks_with_credit = isset($att['credit']) && (!isset($size) || 'full' === $size); ?>
 <?php if (isset( $att['fileUrl'] ) ) : ?>
   <div class="wp-media-wrapper">
     <img src="<?php echo esc_attr( $att['fileUrl'] ); ?>"/>
   </div>
-  <?php if ( isset( $att['caption'] ) || isset( $att['credit']) ) : ?>
+  <?php if ( $nh3_blocks_with_caption || $nh3_blocks_with_credit ) : ?>
     <figcaption>
       <!-- Caption -->
-      <?php if ( isset( $att['caption'] ) ) : ?>
+      <?php if ( $nh3_blocks_with_caption ) : ?>
         <p class="post-block__media-caption"><?php echo esc_html( $att['caption'] ); ?></p>
       <?php endif; ?>
       <!-- Credit -->
-      <?php if ( isset( $att['credit'] ) ) : ?>
+      <?php if ( $nh3_blocks_with_credit ) : ?>
         <p class="post-block__media-credits">
           <a href="<?php echo esc_url( NH3_BLOCKS_SITE_URLS[$att['platform']] ); ?>/entries/<?php echo esc_attr( $att['hash'] ); ?>" target="_blank">
             <?php echo esc_html( $att['credit'] ); ?>
