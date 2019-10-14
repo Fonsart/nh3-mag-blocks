c843fbcc6496688ad08a54c5d0e8eb65b0d01d90
Lecorney Stéphane <stephane.lecorney@heig-vd.ch>
2019-10-14 17:07:49 +0200

    Fixes #35 change on topic view

--- templates/on-topic-section.php
+++ templates/on-topic-section.php
@@ -7,7 +7,7 @@
 
 <?php if ( isset( $content ) && ! empty( trim( $content ) ) ) : ?>
   <section class="on-topic-wrapper">
-    <h2><?php esc_html_e( 'On Topic', 'nh3-mag-blocks' ); ?></h2>
+    <h2><?php esc_html_e('To consult also on notreHistoire.ch', 'nh3-mag-blocks'); ?></h2>
     <!-- Inner blocks content -->
     <?php echo $content; // phpcs:ignore ?>
   </section>
